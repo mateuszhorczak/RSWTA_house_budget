b73ac86f3d06d52ec2ddff36e54f5789318f0f9b
Mateusz Horczak <82621@student.pb.pl>
2023-04-25 13:30:55 +0200

    models

--- expenses/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-# Create your models here.
-
-class Expense(models.Model):
-    name = models.TextField()
-    description = models.TextField()
-    cost = models.TextField()
---
+++ mainapp/models.py
@@ -0,0 +1,24 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+user_model = get_user_model
+
+
+# Create your models here.
+
+class Wallet:
+    name = models.TextField()
+
+
+class Category:
+    name = models.TextField()
+    id_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
+
+
+class Expense(models.Model):
+    name = models.TextField()
+    description = models.TextField()
+    cost = models.FloatField()
+    id_user = models.ForeignKey(user_model, on_delete=models.CASCADE)
+    id_category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    id_wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE)
