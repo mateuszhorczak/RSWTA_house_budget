de208b088e6f496a98abb3fc12023b19aaee2476
Mateusz Horczak <82621@student.pb.pl>
2023-05-29 00:50:36 +0200

    admin panel feature

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,8 +1,30 @@
 from django.contrib import admin
+from django.contrib.admin import AdminSite
+from django.contrib.admin.forms import AdminAuthenticationForm
 
 # Register your models here.
 
 from . import models
+
+
+class CustomAuthenticationForm(AdminAuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['username'].label = 'Nazwa administratora'
+        self.fields['username'].widget.attrs['placeholder'] = 'Wprowadź nazwę administratora'
+        self.fields['password'].label = 'Hasło'
+        self.fields['password'].widget.attrs['placeholder'] = 'Wprowadź hasło'
+
+
+class CustomAdminSite(AdminSite):
+    login_form = CustomAuthenticationForm
+    site_title = 'Panel administracyjny'
+    site_header = 'Panel administracyjny'
+    site_url = '/mainapp/home'
+
+
+admin_site = CustomAdminSite(name='admin')
+admin.site = admin_site
 
 admin.site.register(models.Category)
 admin.site.register(models.IncomeOperation)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 user_model = get_user_model()
 
